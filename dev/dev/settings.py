45dac1dbbf48fbb96c5546c7419d9a7095bb509c
@Youness_Elbrag <younsselbrag@gmail.com>
2023-02-07 19:57:11 +0100

    finishd setup dataBased and add to admin area

--- dev/dev/settings.py
+++ dev/dev/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = "dev.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "houselist",
+        "USER":"postgres",
+        "PASSWORD":"younssblack",
+        "host":"localhost"
     }
 }
 
--- dev/realtors/models.py
+++ dev/realtors/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from datetime import datetime as date
 
-# Create your models here.
+class Realtor(models.Model):
+    """
+    ID is automatically generated from foreign key
+    """
+    name = models.CharField(max_length=20)
+    phone = models.ImageField(upload_to="photo_main/%Y/%m/d/")
+    description = models.TextField(blank=True)
+    phone = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
+    is_mvp = models.BooleanField(default=False)
+    hire_date = models.DateField(default=date.now,blank=True)
+    def __str__(self):
+        return self.name
